636e2670d385377de087f1d645d5e526ef1e37a6
Davi <davilopesmirand@gmail.com>
2026-09-17 11:19:59 -0300

    Commit do projeto de "O Mosaico do Castelo" como atividade complementar da matéria de Algoritmos e programação.

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 public class Main {
     public static void main() {
         Scanner sc = new Scanner(System.in);
-        int tamanhoMatriz = 0;
+        int n = 0;
+        //n é o tamanho que a matriz terá
         do {
             System.out.println("Diga o tamanho do matriz que seja maior ou igual a 3 e menor ou igual a 20: ");
-            tamanhoMatriz = sc.nextInt();
+            n = sc.nextInt();
 
-        } while (tamanhoMatriz <= 3 || tamanhoMatriz >= 20);
-        int matriz[][] = new int[tamanhoMatriz][tamanhoMatriz];
+        } while (n <= 3 || n >= 20);
+        int matriz[][] = new int[n][n];
 
-        System.out.println("DIMENSÃO " +  tamanhoMatriz + " x " + tamanhoMatriz );
-        for (int i = 0; i < tamanhoMatriz; i++) {
-            for (int j = 0; j < tamanhoMatriz; j++) {
+        System.out.println("DIMENSÃO " +  n + " x " + n );
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 matriz[i][j] = i;
                 if (    (i == 0 && j == 0) ||
-                        (i == 0 && j == tamanhoMatriz - 1) ||
-                        (i == tamanhoMatriz - 1 && j == 0) ||
-                        (i == tamanhoMatriz - 1 && j == tamanhoMatriz - 1)  ) {
+                        (i == 0 && j == n - 1) ||
+                        (i == n - 1 && j == 0) ||
+                        (i == n - 1 && j == n - 1)  ) {
                     System.out.print("+ ");
-                }else if (i == 0 && ( j != 0 || j != tamanhoMatriz - 1)
-                        || i == tamanhoMatriz - 1 && ( j != 0 || j != tamanhoMatriz - 1)
-                        || j == 0 && ( i != 0 || i != tamanhoMatriz - 1)
-                        || j == tamanhoMatriz - 1 && ( i != 0 || i != tamanhoMatriz - 1)){
-                    System.out.printf("# ");
+                }else if (i == 0 && ( j != 0 || j != n - 1)
+                        || i == n - 1 && ( j != 0 || j != n - 1)
+                        || j == 0 && ( i != 0 || i != n - 1)
+                        || j == n - 1 && ( i != 0 || i != n - 1)){
+                    System.out.print("# ");
                 } else System.out.print(". ");
             }
             System.out.println();
